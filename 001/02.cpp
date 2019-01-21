cccedd027690e4b81ee4fac4f012017b56196596
yuanrui <yuanrui@sensetime.com>
2019-01-21 21:26:20 +0800

    feat: page 1 solution

---
+++ 001/02.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+#include <cassert>
+
+using namespace std;
+
+class MyStack {
+    stack<int> stackData;
+    stack<int> stackMin;
+public:
+    void push(int d) {
+        stackData.push(d);
+        if (stackMin.empty()) {
+            stackMin.push(d);
+        } else if (d <= stackMin.top()) {
+            stackMin.push(d);
+        } else {
+            stackMin.push(stackMin.top());
+        }
+    }
+    
+    int pop() {
+        stackMin.pop();
+        int value = stackData.top();
+        stackData.pop();
+        return value;
+    }
+
+    int getMin () {
+        if (stackMin.empty()) {
+            cerr << "Current stack has no data" << endl;
+            exit(1);
+        }
+        return stackMin.top();
+    }
+};
+
+int main () {
+    MyStack stack;
+    stack.push(3);
+    assert(stack.getMin() == 3);
+    stack.push(4);
+    assert(stack.getMin() == 3);
+    stack.push(5);
+    stack.push(1);
+    assert(stack.getMin() == 1);
+    stack.push(2);
+    stack.push(1);
+    assert(stack.getMin() == 1);
+    stack.pop();
+
+    cout << "test pass" << endl;
+    return 0;
+}
